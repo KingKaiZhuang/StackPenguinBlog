5c9083ffd3198a234fd1d1031df6459c14da5749
Zhuang Jun Kai <5b0g0024@stust.edu.tw>
2026-06-03 21:34:35 +0800

    chore: fix favicon caching and update configs

--- src/constants/icon.ts
+++ src/constants/icon.ts
@@ -3,6 +3,6 @@ import type { Favicon } from "@/types/config.ts";
 
 export const defaultFavicons: Favicon[] = [
     {
-        src: "/favicon/favicon.ico"
+        src: "/favicon/favicon.ico?v=2"
     }
 ];
