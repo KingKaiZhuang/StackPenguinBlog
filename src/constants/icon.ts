bf55e2fdf9edef965a0f6390ddf5582ad194a53e
Zhuang Jun Kai <5b0g0024@stust.edu.tw>
2026-06-03 21:40:48 +0800

    chore: rename favicon to bypass Vercel edge cache

--- src/constants/icon.ts
+++ src/constants/icon.ts
@@ -3,6 +3,6 @@ import type { Favicon } from "@/types/config.ts";
 
 export const defaultFavicons: Favicon[] = [
     {
-        src: "/favicon/favicon.ico?v=2"
+        src: "/favicon/penguin.ico"
     }
 ];
